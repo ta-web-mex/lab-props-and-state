a629d6849878aa0b0c3b11b2ff948472e5ddf73e
oscarmtzo <icehman@hotmail.com>
2019-07-16 09:21:33 -0500

    iteracion 1 casi completa

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Form/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,38 @@
 import React, {Component} from 'react'
 
 class Form extends Component {
-    state={
-        
+    inputChange = event => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+    inputChangeT=event => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+    render(){
+        return(
+            <div>
+                <input 
+                type="text"
+                onChange={this.inputChange}
+                value={this.inputChange.text}
+                />
+                <p>{this.inputChange.text}</p>
+                <input
+                type="text"
+                onChange={this.inputChangeT}
+                value={this.inputChangeT.text}
+                />
+                <p></p>
+                <input 
+                type="text"
+                onChange={this.inputChangeT}
+                value={this.inputChangeT.text}/>
+                <p></p>
+            </div>
+        )
     }
 }
+export default Form
