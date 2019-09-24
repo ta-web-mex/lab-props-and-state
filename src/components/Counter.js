cf4e51cb0eeba43c968485bcebe4f5435dbc0c86
k4i5er <kaiser@LAPTOP-CO6Q31HQ.localdomain>
2019-09-24 01:17:00 -0500

    Styles added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,9 +2,12 @@ import React from "react"
 
 const Counter = (props) => (
   <div>
+    <h1>Count</h1>
     <h2>{props.count}</h2>
-    <button onClick={props.plus}>+</button>
-    <button onClick={props.minus}>-</button>
+    <div id="buttons">
+      <button onClick={props.plus}>+</button>
+      <button onClick={props.minus}>-</button>
+    </div>
   </div>
 )
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,17 @@ import React from "react"
 const Form = (props) => {
   return (
     <div>
-      <input type="text" id="name" name="name" onChange={props.handleChange} />
-      <input type="text" id="surname" name="surname" onChange={props.handleChange} />
-      <input type="text" id="age" name="age" onChange={props.handleChange} />
-      <p>{props.name}</p>
-      <p>{props.surname}</p>
-      <p>{props.age}</p>
+      <h1>Form</h1>
+      <div id="buttons">
+        <input type="text" id="name" name="name" onChange={props.handleChange} />
+        <input type="text" id="surname" name="surname" onChange={props.handleChange} />
+        <input type="text" id="age" name="age" onChange={props.handleChange} />
+      </div>
+      <div id="ps">
+        <p>{props.name}</p>
+        <p>{props.surname}</p>
+        <p>{props.age}</p>
+      </div>
     </div>
   )
 }
