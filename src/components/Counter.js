a5045c6c774c33854a1677f8a387f6a279f9394e
FranciscoEP <francisco.eugp@gmail.com>
2020-05-04 23:17:14 -0500

    styles added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Counter = ({ plus, min, count = 0 }) => (
   <div>
-    <h2>Counter</h2>
-    <h2>{count}</h2>
+    <h2>
+      <strong>Counter</strong>
+    </h2>
+    <h2>
+      <strong>{count}</strong>
+    </h2>
     <button onClick={min}>-</button>
     <button onClick={plus}>+</button>
   </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const Form = ({ name, surname, age, changeInputName, changeInputSurname, changeInputAge }) => (
-  <div>
+  <div class="form">
     <input type="text" name="name" value={name} onChange={changeInputName} />
-    <input type="text" name="surname" value={surname} onChange={changeInputSurname} />
-    <input type="text" name="age" value={age} onChange={changeInputAge} />
     <p>{name}</p>
+    <input type="text" name="surname" value={surname} onChange={changeInputSurname} />
     <p>{surname}</p>
+    <input type="text" name="age" value={age} onChange={changeInputAge} />
+
     <p>{age}</p>
   </div>
 )
